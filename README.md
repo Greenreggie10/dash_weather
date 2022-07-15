d7e15bea0662216067e79705b158e396bfd73f0c
Greenreggie10 <Reggie.green10@yahoo.com>
2022-07-14 23:42:03 -0400

    city location

--- README.md
+++ README.md
@@ -7,15 +7,21 @@
 
 <!-- URL to hosted site -->
 [live demo](https://greenreggie10.github.io/dash_weather/);
+*Type & search the city of your choice*
 
 <!-- picture of site -->
 ![alt text]();
 
 <!-- description of the site purpose -->
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+This ia a webpage with APIs allowing developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Challenge six is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+
+<!--Testing && Results -->
+
 
 <!-- author & contributors -->
 REGINALD GREEN GITHUB@GREENREGGIE10
+
+Email:Reggie.green10@yahoo.com
 
 <!-- updates -->
 "inital"
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,74 @@
 //THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 //WHEN I click on a city in the search history
 //THEN I am again presented with current and future conditions for that city
+
+
+
+
+var searchBtn = document.getElementById("search");
+
+searchBtn.addEventListener("click",function(event){
+    var API = "286de79db450c76b40b056833d811f67";
+    var citySearch = document.getElementById("city-search").value; 
+    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+citySearch+"&appid="+ API;
+    console.log("citySearch clicked",citySearch);
+    fetch(requestUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        var latitude = data[0].lat;
+        var longitude = data[0].lon;
+        var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='+ latitude +'&lon='+longitude+'&appid='+ API;
+    fetch(requestUrl)
+    .then(function(response) {
+        return response.json();
+        })
+    .then(function(data) {
+        console.log("line35",data);
+    });
+    });
+    
+
+
+});    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// remove all tasks
+// $("#remove-tasks").on("click", function() {
+//   for (var key in tasks) {
+//     tasks[key].length = 0;
+//     $("#list-" + key).empty();
+//   }
+//   saveTasks();
+// });
+
+// // load tasks for the first time
+// loadTasks();
